9af0b99013627d3b366ab6e6f854321c07bdaa07
seregaNV <serega.n.v333@gmail.com>
2016-05-04 00:47:39 +0300

    5

--- app_console/controllers/parseController.js
+++ app_console/controllers/parseController.js
@@ -4,6 +4,10 @@ const parseMan = require('../managers/parseManager');
 var request = require('request'),
 	Q = require("q"),
 	cheerio = require('cheerio'),
+	Iconv = require('iconv').Iconv,
+	fromEnc = 'cp1251',
+	toEnc = 'utf-8',
+	translator = new Iconv(fromEnc,toEnc),
 	url = 'http://www.infocar.ua/reviews/acura/mdx/2008/3.7-avtomat-suv-id22068.html';
 /**
  * @module controllers/parseController
@@ -21,20 +25,58 @@ module.exports = {
 		//	}
 		//})
 
-		request(url, function(err, res, body){
+		request({
+			url: url,
+			encoding: null
+		}, function(err, res, body){
 			if(err)console.log(err);
 			else{
-				$ = cheerio.load(body);
-				var cards = [];
-				$('.card').each(function(){
+				var $ = cheerio.load(translator.convert(body));
+				var reviews = [];
+				$('.description').each(function(){
 					cards.push({
-						title:$('.title',this).text(),
-						url:$('a',this).attr('href')
+						description:$('.description').text()
+						//url:$('a',this).attr('href')
 					});
 				});
+				console.log('reviews - ', reviews);
 			}
-			console.log('cards - ', cards);
 		});
+
+
+
+		////stage 1
+		//request({
+		//		url: url,
+		//		encoding: null
+		//	}, function(err,res, body){
+        //
+		//	var $ = cheerio.load(translator.convert(body));
+		//	var pager = $('.pager');
+		//	var limitPage = parseInt( pager.eq(pager.length-1).text().trim(), 10);
+		//	//stage 2
+		//	function parsePage(page){
+		//		var defer = Q.defer();
+		//		request('/pager/'+page,function(err,res, body){
+		//			if(page<=limitPage){
+		//				defer.resolve(page+1); //инкрементируем счетчик страниц прямо в асинхронной последовательности передавая его в качестве аргумента следующим вызовам
+		//			} else {
+		//				defere.reject();
+		//			}
+		//			//тут код из первого абстракного примера
+		//		});
+		//		//возвращаем promise чтобы на нем построить последовательность.
+		//		return defer.promise;
+		//	}
+		//	var chain = Q.fcall(function(){
+		//		return parsePage(1);
+		//	});
+		//	for(var i = 2; i<limitPage;i++){
+		//		chain = chain.then(function(page){
+		//			return parsePage(page); // цепляем в цепь новые задачи на парсинг, как старые выполнятся.
+		//		});
+		//	}
+		//});
 
 
 	}
