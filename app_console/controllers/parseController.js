0e1206f0e94a687d7e49d97a83021b7641d96db2
seregaNV <serega.n.v333@gmail.com>
2016-05-04 23:36:56 +0300

    7

--- app_console/controllers/parseController.js
+++ app_console/controllers/parseController.js
@@ -1,95 +1,86 @@
 "use strict";
 
 const parseMan = require('../managers/parseManager');
-var request = require('request'),
-	Q = require("q"),
-	cheerio = require('cheerio'),
-	Iconv = require('iconv').Iconv,
-	fromEnc = 'cp1251',
-	toEnc = 'utf-8',
-	translator = new Iconv(fromEnc,toEnc),
-	url = 'http://www.infocar.ua/reviews/acura/mdx/2008/3.7-avtomat-suv-id22068.html';
+
 /**
  * @module controllers/parseController
  * @exports parse/parseController
  */
 module.exports = {
 
-	parse: function * (options) {
-		console.log('test_controller - ' + options.count);
-		yield parseMan.parse(options.count);
+	/**
+	 * cd /var/www/news_koa_node; /usr/bin/node --harmony ./console.js --section test --action pars
+	 * cd /home/student/Documents/work/parse; /usr/bin/node --harmony ./console.js --section parse --action parse --opt.count=1000
+	 * ./console.js --section parse --opt.count=1000
+	 * @param options
+	 */
+	parse : function* (options) {
+		var parseData,
+			resource = ['infocar.ua', 'autonavigator.ru'],
+		// url= 'http://www.infocar.ua/reviews/acura/mdx/2008/3.7-avtomat-suv-id22068.html',
+		// url= 'http://www.infocar.ua/reviews/toyota/corolla/2011/1.3-mehanika-sedan-id22162.html', // дописано
+		// url= 'http://www.infocar.ua/reviews/toyota/corolla/2011/1.3-mehanika-sedan-id21142.html', // дописано + совет
+		// url= 'http://www.infocar.ua/reviews/toyota/corolla/2008/1.6-mehanika-sedan-id20625.html', // "-"
+		// url= 'http://www.infocar.ua/reviews/toyota/corolla/2013/1.6-mehanika-sedan-id20398.html', // дописано
 
-		//request(url, function (error, response, body) {
-		//	if (!error && response.statusCode == 200) {
-		//		console.log('body - ', body); // Show the HTML for the Google homepage.
-		//	}
-		//})
-
-		request({
-			url: url,
-			encoding: null
-		}, function(err, res, body){
-			if(err)console.log(err);
-			else{
-				var $ = cheerio.load(translator.convert(body));
-				var reviews = [];
-				$('.description').each(function(){
-					reviews.push({
-						description: $('.description').text()
-						//url:$('a',this).attr('href')
-					});
-				});
-				$('.rates i').each(function(){
-					var obj = {};
-					obj[$('.rates i').text()] = $('.rates span').attr('title');
-					reviews.push(obj);
-					//reviews.push({
-						//$('.rates i').text()[$('.rates').text()]
-					//rates: $('.rates').text()
-						//url:$('a',this).attr('href')
-					//});
-				});
-				console.log('reviews - ', reviews);
-			}
-		});
-
-
-
-		////stage 1
-		//request({
-		//		url: url,
-		//		encoding: null
-		//	}, function(err,res, body){
-        //
-		//	var $ = cheerio.load(translator.convert(body));
-		//	var pager = $('.pager');
-		//	var limitPage = parseInt( pager.eq(pager.length-1).text().trim(), 10);
-		//	//stage 2
-		//	function parsePage(page){
-		//		var defer = Q.defer();
-		//		request('/pager/'+page,function(err,res, body){
-		//			if(page<=limitPage){
-		//				defer.resolve(page+1); //инкрементируем счетчик страниц прямо в асинхронной последовательности передавая его в качестве аргумента следующим вызовам
-		//			} else {
-		//				defere.reject();
-		//			}
-		//			//тут код из первого абстракного примера
-		//		});
-		//		//возвращаем promise чтобы на нем построить последовательность.
-		//		return defer.promise;
-		//	}
-		//	var chain = Q.fcall(function(){
-		//		return parsePage(1);
-		//	});
-		//	for(var i = 2; i<limitPage;i++){
-		//		chain = chain.then(function(page){
-		//			return parsePage(page); // цепляем в цепь новые задачи на парсинг, как старые выполнятся.
-		//		});
-		//	}
-		//});
-
-
+		// url= 'http://www.autonavigator.ru/reviews/Toyota/Corolla/34586.html',
+		// url = 'http://www.autonavigator.ru/reviews/Toyota/Corolla/34864.html',
+		// url = 'http://www.autonavigator.ru/reviews/Toyota/Corolla/35018.html',
+			url = 'http://www.autonavigator.ru/reviews/Toyota/Corolla/35602.html',
+			data = yield parseMan.getDataForParse(url);
+		switch (resource[1]) {
+			case 'infocar.ua':
+				parseData = yield parseMan.parseDataInfocar(data);
+				break;
+			case 'autonavigator.ru':
+				parseData = yield parseMan.parseDataAutonavigator(data);
+				break;
+			default:
+				console.log('other resource!');
+		}
+		console.log(parseData);
 	}
+
+
+
+	//parse: function * (options) {
+	//	console.log('test_controller - ' + options.count);
+	//	yield parseMan.parse(options.count);
+    //
+	//	//request(url, function (error, response, body) {
+	//	//	if (!error && response.statusCode == 200) {
+	//	//		console.log('body - ', body); // Show the HTML for the Google homepage.
+	//	//	}
+	//	//})
+    //
+	//	request({
+	//		url: url,
+	//		encoding: null
+	//	}, function(err, res, body){
+	//		if(err)console.log(err);
+	//		else{
+	//			var $ = cheerio.load(translator.convert(body));
+	//			var reviews = [];
+	//			$('.description').each(function(){
+	//				reviews.push({
+	//					description: $('.description').text()
+	//					//url:$('a',this).attr('href')
+	//				});
+	//			});
+	//			$('.rates i').each(function(){
+	//				var obj = {};
+	//				obj[$('.rates i').text()] = $('.rates span').attr('title');
+	//				reviews.push(obj);
+	//				//reviews.push({
+	//					//$('.rates i').text()[$('.rates').text()]
+	//				//rates: $('.rates').text()
+	//					//url:$('a',this).attr('href')
+	//				//});
+	//			});
+	//			console.log('reviews - ', reviews);
+	//		}
+	//	});
+
 
 };
 
--- app_console/managers/parseManager.js
+++ app_console/managers/parseManager.js
@@ -5,8 +5,14 @@
 	const elasticsearch = require('elasticsearch');
 
 	let elasticConfig = require('../../config/elasticsearch'),
-		client = new elasticsearch.Client(elasticConfig.busearch);
-
+		client = new elasticsearch.Client(elasticConfig.busearch),
+		Q = require('q'),
+		request = require('request'),
+		cheerio = require('cheerio'),
+		Iconv = require('iconv').Iconv,
+		fromEnc = 'cp1251',
+		toEnc = 'utf-8',
+		translator = new Iconv(fromEnc,toEnc);
 	module.exports = {
 
 		/**
@@ -15,9 +21,143 @@
 		 * @description Вычисляет общее количество запросов
 		 * @returns {{totalQueries: int}}
 		 */
-		parse: function* parse(count) {
-			console.log('test_manager - ' + count);
+		//parse: function* parse(count) {
+		//	console.log('test_manager - ' + count);
+		//}
+
+		getDataForParse: function(url) {
+			var deferred = Q.defer();
+			request({
+				url: url,
+				encoding: null
+			}, function(err, res, body){
+				if(err)deferred.reject(false);
+				else deferred.resolve(body);
+			});
+			return deferred.promise;
+		},
+
+		parseDataInfocar: function(data) {
+			var $ = cheerio.load(translator.convert(data)),
+				reviews = {};
+
+			// добавляємо назву
+			reviews.title = $('h1.summary').text();
+
+			// добавляємо автора
+			var autorArr = {};
+			var user = $('.user');
+			autorArr.name = user.find('a > span').text();
+			// autorArr.country = user.find('span.adr > span.country-name').text(); // питання ??? чи нада вказувати страну
+			autorArr.city = user.find('span.adr > span.locality').text();
+			autorArr.date = user.find('span.dtreviewed').text();
+			reviews.autor = autorArr;
+
+			// добавляємо авто
+			var autoArr = {};
+			$('.car > p > i').each(function(){
+				autoArr[$(this).text().slice(0, -1)] = $(this).next().text();
+			});
+			reviews.auto = autoArr;
+
+			// добавляємо опис і те, що користувач дописав
+			var descr = [];
+			$('.description').each(function(){
+				descr.push($('.description').text() );
+			});
+			$('div.hreview > p > strong').each(function(){
+				var container = $(this).parent();
+				if (!container.attr('id')) {
+					descr.push(container.text() );
+				}
+			});
+			// var newDescr = descr.map(function(item) {
+			//   var reg = /(\\[rn])/g;
+			//   return item.replace(reg, " ");
+			// });
+			// reviews.push(newDescr);
+			reviews.description = descr;
+
+			// добавляємо плюси
+			reviews.plus = $('#plus span.pro').text();
+
+			// добавляємо минуси
+			reviews.minus = $('#minus span.contra').text();
+
+			// добавляємо совєти
+			reviews.advice = $('#sovet > span').text();
+
+			// добавляємо бали
+			var ratesArr = [];
+			$('.rates span').each(function(){
+				ratesArr.push($(this).attr('title'));
+			});
+			reviews.rates = ratesArr;
+
+			return reviews;
+
+		},
+
+		parseDataAutonavigator: function(data) {
+			var $ = cheerio.load(translator.convert(data)),
+				reviews = {};
+
+			// добавляємо назву
+			reviews.title = $('h1').text();
+
+			// var infoContainer = $('.brief, .item, .hproduct');
+			// добавляємо автора
+			var autorArr = {};
+			var user = $('.brief, .item, .hproduct').find('.meta');
+			autorArr.name = user.find('a.name').text();
+			// autorArr.country = '';
+			autorArr.city = user.find('span.locality').text();
+			autorArr.date = user.find('div.date').text();
+			reviews.autor = autorArr;
+
+			// добавляємо авто
+			var autoArr = {};
+			$('.hproduct > p').each(function(){
+				/([\s\dа-яА-ЯёЁ\-.]*):([\s\dа-яА-ЯёЁ\-.,]*)/.exec($(this).text());
+				autoArr[RegExp.$1] = RegExp.$2;
+				// autoArr.push($(this).text()); // можна прощє, але в вигляді масіва. Поміняти (var autoArr = [])
+			});
+			reviews.auto = autoArr;
+
+			// добавляємо опис
+			var descr = [];
+			$('.description').each(function(){
+				descr.push($('.description').text() );
+			});
+			reviews.description = descr;
+
+			// добавляємо плюси
+			reviews.plus = $('.plus > p').text();
+
+			// добавляємо минуси
+			reviews.minus = $('.minus > p').text();
+
+			// добавляємо совєти
+			reviews.advice = $('.advice > p').text();
+
+			// добавляємо бали
+			var ratesArr = [];
+			$('.allStars div').each(function(){
+				var parseValue,
+					value = $(this).find('span > i').attr('style');
+				if (value) {
+					parseValue = Number(value.replace(/\D+/g,""))/20;
+				} else {
+					parseValue = $(this).find('span').text();
+				}
+				ratesArr.push($(this).find('label').text() + ': ' + parseValue);
+			});
+			reviews.rates = ratesArr;
+
+			return reviews;
+
 		}
+
 	};
 
 }());
